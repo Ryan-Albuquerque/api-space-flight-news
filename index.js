4c933a0a870b051fa6b330f229962cd2b2e0af26
Ryan Albuquerque <56835754+Ryan-Albuquerque@users.noreply.github.com>
2022-02-11 01:28:47 -0400

    routes, list route, http status enum

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors');
 
 const database = require('./src/database');
+const routes = require('./src/routes');
 
 const app = express()
 const port = process.env.PORT || 8080;
@@ -9,6 +10,7 @@ const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 
+app.use(routes);
 
 app.listen(port, async () => {
     await database.prototype.connect();
